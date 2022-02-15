72744f98b9c11552d6ffa47a218a0f71e47b3e1c
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-02-15 21:29:26 +0100

    episode 4 tutorial

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,26 +1,26 @@
 export default class Timer {
-    constructor(deltaTime = 1/60){
-
+    constructor(deltaTime = 1/60) {
         let accumulatedTime = 0;
         let lastTime = 0;
 
         this.updateProxy = (time) => {
-            accumulatedTime += (time-lastTime)/1000;
+            accumulatedTime += (time - lastTime) / 1000;
 
-            while (accumulatedTime > deltaTime){
+            while (accumulatedTime > deltaTime) {
                 this.update(deltaTime);
-                accumulatedTime-=deltaTime;
+                accumulatedTime -= deltaTime;
             }
-     
-        lastTime = time;
-       
-        this.enqueue();
-    }
-}
 
-enqueue() {
-    requestAnimationFrame(this.updateProxy);
-}
+            lastTime = time;
+
+            this.enqueue();
+        }
+    }
+
+    enqueue() {
+        requestAnimationFrame(this.updateProxy);
+    }
+
     start() {
         this.enqueue();
     }
