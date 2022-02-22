71451307b252a8120b87e9781ca50f7d781b0009
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-02-22 10:14:43 +0100

    new backend folder springboot try

---
+++ backend/SuperMarion/SuperMarion/src/main/java/se/marion/SuperMarion/SuperMarionApplication.java
@@ -0,0 +1,13 @@
+package se.marion.SuperMarion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarionApplication.class, args);
+	}
+
+}
---
+++ backend/SuperMarion/SuperMarion/src/test/java/se/marion/SuperMarion/SuperMarionApplicationTests.java
@@ -0,0 +1,13 @@
+package se.marion.SuperMarion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
